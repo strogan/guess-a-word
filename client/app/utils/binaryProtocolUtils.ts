fff4e8ee315e4d9300900b3caee1d2b7ce2993de
strogan <asdof01@gmail.com>
2024-03-25 07:06:32 +0100

    binary protocol update

--- client/app/utils/binaryProtocolUtils.ts
+++ client/app/utils/binaryProtocolUtils.ts
@@ -1,14 +1,16 @@
-//utils
-export function encodeMessage(data: Object) {
+export function encodeMessage(data: Object): Buffer {
   const str = JSON.stringify(data);
-  const buf = Buffer.from(str, "utf8");
-  return buf
+  const lenBuf = Buffer.alloc(4);
+  lenBuf.writeInt32LE(str.length, 0);
+  const strBuf = Buffer.from(str, 'utf8');
+  return Buffer.concat([lenBuf, strBuf]);
 }
 
-export function decodeMessage(data: string) {
-  const buffer = Buffer.from(data);
-  const str = buffer.toString('utf8');
-  const obj = JSON.parse(str);
-
-  return obj
+export function decodeMessage(data: ArrayBuffer) {
+  const buffer = new Uint8Array(data);
+  const view = new DataView(buffer.buffer);
+  const length = view.getInt32(0, true);
+  const str = Buffer.from(buffer.buffer).toString('utf8', 4, length + 4);
+  let obj = JSON.parse(str)
+  return obj;
 }
--- server/utils/coder.js
+++ server/utils/coder.js
@@ -1,14 +1,16 @@
-//utils
 export function encodeMessage(data) {
-    const str = JSON.stringify(data);
-    const buf = Buffer.from(str, "utf8");
-    return buf
-  }
-  
-  export function decodeMessage(data) {
-    const buffer = Buffer.from(data.data);
-    const str = buffer.toString('utf8');
-    const obj = JSON.parse(str);
-  
-    return obj
-  }
+  const str = JSON.stringify(data);
+  const lenBuf = Buffer.alloc(4);
+  lenBuf.writeInt32LE(str.length, 0);
+  const strBuf = Buffer.from(str, 'utf8');
+  return Buffer.concat([lenBuf, strBuf]);
+}
+
+export function decodeMessage(data) {
+  const buffer = Buffer.from(data);
+  const length = buffer.readInt32LE(0);
+  const str = buffer.toString('utf8', 4, length + 4);
+
+  const obj = JSON.parse(str);
+  return obj;
+}
